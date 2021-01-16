a239e23a1c825475c9e992fd90b69dd9752ca576
Taral Patel <taralpatel610@gmail.com>
2021-01-15 19:38:15 -0500

    Service worker updates

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ self.addEventListener("install", function ( event ) {
     event.waitUntil(
         caches.open("jsmonthly").then(function (cache) {
             cache.addAll([
-                "/index.html",
-                "/styles.css"
+                "/index.html"
             ])
         })
     );
@@ -15,18 +14,3 @@ self.addEventListener("fetch", function (event) {
             return cache.match( event.request );
         }))
 });
-
-self.addEventListener("beforeinstallprompt", e => {
-    console.log("beforeinstallprompt Event fired");
-    e.preventDefault();
-    this.deferredPrompt = e;
-    return false;
-});
-  
-this.deferredPrompt.prompt();
-
-this.deferredPrompt.userChoice.then(choice => {
-    console.log(choice);
-});
-
-this.deferredPrompt = null;
