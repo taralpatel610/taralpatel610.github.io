b8e3e04fe89d30e73fd577f7eba4439483ab7314
Taral Patel <taralpatel610@gmail.com>
2021-01-15 19:43:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
-self.addEventListener("install", function ( event ) {
-    event.waitUntil(
-        caches.open("jsmonthly").then(function (cache) {
-            cache.addAll([
-                "/index.html"
-            ])
+self.addEventListener("install", function (e) {
+    console.log("Service worker installation");
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log("Alloy service worker caching dependencies");
+            initialCache.map(function(url) {
+                return cache.add(url).catch(function(reason) {
+                    return console.log(
+                        "Alloy: " + String(reason) + " " + url
+                    );
+                });
+            });
         })
     );
 });
 
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        caches.open("jsmonthly").then(function ( cache ) {
-            return cache.match( event.request );
-        }))
+self.addEventListener("activate", function(e) {
+    console.log("Service worker activation");
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            return Promise.all(
+                keyList.map(function(key) {
+                    if (key !== cacheName) {
+                        console.log("Old cache removed", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+    if (new URL(e.request.url).origin !== location.origin) return;
+
+    if (e.request.mode === "navigate" && navigator.onLine) {
+        e.respondWith(
+            fetch(e.request).then(function(response) {
+                return caches.open(cacheName).then(function(cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            })
+        );
+        return;
+    }
+
+    e.respondWith(
+        caches
+            .match(e.request)
+            .then(function(response) {
+                return (
+                    response ||
+                    fetch(e.request).then(function(response) {
+                        return caches.open(cacheName).then(function(cache) {
+                            cache.put(e.request, response.clone());
+                            return response;
+                        });
+                    })
+                );
+            })
+            .catch(function() {
+                return caches.match(offlinePage);
+            })
+    );
 });
